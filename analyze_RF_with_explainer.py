939b7f3635210a993e9472c8f55d7c344eca7c73
j <jgwak1@binghamton.edu>
2023-12-19 20:59:02 -0500

    pritis files

---
+++ analyze_RF_with_explainer.py
@@ -0,0 +1,327 @@
+import os
+import sys
+import torch
+import gc
+# import pytorch_lightning as pl
+import numpy as np
+import re
+from collections import Counter
+
+'''
+Replaced 'code' to 'source' as there's conflict with vscode debugger.
+from code.dataprocessor_graphs import LoadGraphs
+from code.model import GIN
+from code.trainer import TrainModel
+'''
+
+sys.path.append("/data/d1/jgwak1/tabby/graph_embedding_improvement_JY_git/source")
+
+from source.dataprocessor_graphs import LoadGraphs
+from source.model import GIN
+from source.trainer_meng_ver import TrainModel
+
+from itertools import product
+import pandas as pd
+from datetime import datetime
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GroupKFold, StratifiedGroupKFold
+
+
+''' Applying 'Nested Stratified K-fold Cross-Validation' with GAT, GIN to Project-2-Dataset '''
+#SKLEARN: https://scikit-learn.org/stable/modules/classes.html#module-sklearn.model_selectio
+#         https://scikit-learn.org/0.21/modules/model_evaluation.html#classification-metrics
+# splitter-classes
+from pickletools import optimize
+# from grpc import stream_unary_rpc_method_handler
+from sklearn.model_selection import StratifiedKFold, RepeatedStratifiedKFold
+# hyperparameter-optimizers
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+# model-validation
+from sklearn.model_selection import cross_val_score # evaluate score by cross-validation.
+# performance-metrics
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score 
+from sklearn.metrics import make_scorer
+# custom-estimator-class
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+#GNN-TRAINING
+from source.dataprocessor_graphs import LoadGraphs
+#from source.model import GAT, GIN, GIN_no_edgefeat, GIN_no_edgefeat_simplified, GCN, GAT_He, GAT_mlp_fed_1gram 
+#from source.model import GNNBasic, LocalMeanPool,  SumPool, GlobalMeanPool
+
+#from source.gnn_dynamicgraph import GNN_DynamicGraph__ImplScheme_1
+
+# from source.trainer import TrainModel, get_dataloader
+
+#ETC
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.loader import DataLoader #PW nood to install torch_geometric
+torch.set_printoptions(profile="full")
+import numpy as np 
+import random
+from datetime import datetime
+import os
+import json
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+import re
+import gc
+from distutils.util import strtobool
+from argparse import ArgumentParser
+
+import pprint
+from collections import defaultdict
+import time
+#**********************************************************************************************************************************************************************
+
+# silkservice version
+taskname_colnames = [ 
+    'None_or_empty', #0 (index 0) 
+    'Cleanup', #1
+    'Close', #2
+    'Create', #3
+    'CreateNewFile', #4
+    'DeletePath',#5
+    'DirEnum',#6
+    'DirNotify',#7
+    'Flush',#8
+    'FSCTL',#9
+    'NameCreate',#10
+    'NameDelete',#11
+    'OperationEnd',#12
+    #'QueINFO',#13
+    'QueryInformation',#13
+    'QueryEA',#14
+    'QuerySecurity',#15
+    'Read',#16
+    'Write',#17
+    'SetDelete',#18
+    'SetInformation', #19
+    'PagePriorityChange',#20
+    'IoPriorityChange',#21
+    'CpuBasePriorityChange',#22
+    #'IMAGEPriorityChange',#24
+    'CpuPriorityChange',#23
+    'ImageLoad',#24
+    'ImageUnload',#25
+    'ProcessStop/Stop',#26
+    'ProcessStart/Start',#27
+    'ProcessFreeze/Start',#28--------
+    #'PSDISKIOATTRIBUTE',#31
+    #'PSIORATECONTROL',#32 
+    'ThreadStart/Start',#29
+    'ThreadStop/Stop',#30
+    'ThreadWorkOnBehalfUpdate', #31
+    'JobStart/Start',#32--------
+    'JobTerminate/Stop',#33--------
+    #'LOSTEVENT',#38
+    #'PSDISKIOATTRIBUTION',#39
+    'Rename',#34
+    'Renamepath',#35
+    'Thisgroupofeventstrackstheperformanceofflushinghives',#36-------
+    'EventID(1)',#37
+    'EventID(2)',#38
+    'EventID(3)',#39
+    'EventID(4)',#40
+    'EventID(5)',#41
+    'EventID(6)',#42
+    'EventID(7)',#43
+    'EventID(8)',#44
+    'EventID(9)',#45
+    'EventID(10)',#46
+    'EventID(11)',#47
+    'EventID(13)',#48
+    'EventID(14)',#49
+    'EventID(15)',#50
+    'KERNEL_NETWORK_TASK_TCPIP/Datasent.', #51
+    'KERNEL_NETWORK_TASK_TCPIP/Datareceived.',#52
+    'KERNEL_NETWORK_TASK_TCPIP/Connectionattempted.',#53
+    'KERNEL_NETWORK_TASK_TCPIP/Disconnectissued.', #54
+    'KERNEL_NETWORK_TASK_TCPIP/Dataretransmitted.',#55
+    'KERNEL_NETWORK_TASK_TCPIP/connectionaccepted.' , #56----
+    'KERNEL_NETWORK_TASK_TCPIP/Protocolcopieddataonbehalfofuser.', #57
+    'KERNEL_NETWORK_TASK_UDPIP/DatareceivedoverUDPprotocol.',#58
+    'KERNEL_NETWORK_TASK_UDPIP/DatasentoverUDPprotocol.', #59
+    # 'NULL', #remove this entry once second step updations used for subgra
+    'Unseen', # 60
+    #PW: below events are now different in silketw
+    # all below 3 task are combined with opcode and having index 43 onwards for all of them in the function TN2int()
+    # 'KERNEL_NETWORK_TASK_UDPIP'#index 43 # 42(opcode value) 43,49(https://github.com/repnz/etw-providers-docs/blob/master/Manifests-Win7-7600/Microsoft-Windows-Kernel-Network.xml)
+    # 'KERNEL_NETWORK_TASK_TCPIP', # 10-18 (https://github.com/repnz/etw-providers-docs/blob/master/Manifests-Win7-7600/Microsoft-Windows-Kernel-Network.xml)
+    # 'MICROSOFT-WINDOWS-KERNEL-REGISTRY', # 32- 46 (https://github.com/repnz/etw-providers-docs/blob/master/Manifests-Win7-7600/Microsoft-Windows-Kernel-Registry.xml)
+
+]
+
+
+#------------------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------------------------#
+
+# PW 19th Dec,23 below code using for benign flattened graph vs. malware flattened graph comparison
+def get_No_Graph_Structure_eventdist_dict( dataset : list ):
+
+      file_node_tensor = torch.tensor([1, 0, 0, 0, 0])
+      reg_node_tensor = torch.tensor([0, 1, 0, 0, 0])
+      net_node_tensor = torch.tensor([0, 0, 1, 0, 0])
+      proc_node_tensor = torch.tensor([0, 0, 0, 1, 0])
+      thread_node_tensor = torch.tensor([0, 0, 0, 0, 1])
+
+      data_dict = dict()
+
+      for data in dataset:
+
+         data.edge_attr = data.edge_attr[:,:-1] # drop time-scalar
+
+         eventdist = torch.sum(data.edge_attr, dim = 0)
+
+         result_list = [taskname_colnames[i] if value != 0.0 else value for i, value in enumerate(eventdist.tolist())]
+
+
+         data_dict[ re.search(r'Processed_SUBGRAPH_P3_(.*)\.pickle', data.name).group(1) ] = result_list
+
+      return data_dict
+
+
+
+
+def eventdist(benign_data_dict,malware_data_dict):
+    from collections import Counter
+    all_events = set()
+
+    # Collect all unique events from both dictionaries
+    for events_list in benign_data_dict.values():
+        all_events.update(events_list)
+    for events_list in malware_data_dict.values():
+        all_events.update(events_list)
+
+    # Initialize counters for both dictionaries
+    counter_dict1 = Counter()
+    counter_dict2 = Counter()
+
+    # Count occurrences of each event in each dictionary
+    for events_list in benign_data_dict.values():
+        counter_dict1.update(events_list)
+    for events_list in malware_data_dict.values():
+        counter_dict2.update(events_list)
+
+    # Calculate the difference in event counts
+    difference = {event: counter_dict1[event] - counter_dict2[event] for event in all_events}
+
+    # Separate events into more frequent in benign and more frequent in malware
+    more_frequent_in_benign = {event: count for event, count in difference.items() if count > 0}
+    more_frequent_in_malware = {event: -count for event, count in difference.items() if count < 0}
+    common_events_same_count = {event: 0 for event, count in difference.items() if count == 0}
+    # Find common events
+    common_events = {event: 0 for event in difference}
+
+
+    return more_frequent_in_benign, more_frequent_in_malware , common_events_same_count, common_events 
+#**********************************************************************************************************************************************************************
+
+#**********************************************************************************************************************************************************************
+#**********************************************************************************************************************************************************************
+#**********************************************************************************************************************************************************************
+
+#############################################################################################################################
+# libraries for tradition-ML (2023-07-25)
+
+import sklearn
+from sklearn import metrics
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier # XGB
+
+# https://lightgbm.readthedocs.io/en/latest/pythonapi/lightgbm.LGBMClassifier.html#lightgbm.LGBMClassifier
+
+# for baseline
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+##############################################################################################################################
+
+
+
+if __name__ == '__main__':
+    ###############################################################################################################################################
+    # Set data paths
+    projection_datapath_Benign_Train_dict = {
+      # Dataset-1 (B#288, M#248) ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      #PW: Dataset-Case-1 
+      "Dataset-Case-1": \
+         "/data/d1/jgwak1/tabby/SILKETW_benign_train_test_data_case1/offline_train/Processed_Benign_ONLY_TaskName_edgeattr",
+      # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      # Dataset-2 (B#662, M#628)
+      "Dataset-Case-2": \
+         "/data/d1/jgwak1/tabby/SILKETW_benign_train_test_data_case1_case2/offline_train/Processed_Benign_ONLY_TaskName_edgeattr"
+    }
+    projection_datapath_Malware_Train_dict = {
+      # Dataset-1 (B#288, M#248) ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      "Dataset-Case-1": \
+         "/data/d1/jgwak1/tabby/SILKETW_malware_train_test_data_case1/offline_train/Processed_Malware_ONLY_TaskName_edgeattr",
+      # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      # Dataset-2 (B#662, M#628)
+      "Dataset-Case-2": \
+         "/data/d1/jgwak1/tabby/SILKETW_malware_train_test_data_case1_case2/offline_train/Processed_Malware_ONLY_TaskName_edgeattr"
+    }
+    projection_datapath_Benign_Test_dict = {
+      # Dataset-1 (B#73, M#62) ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      "Dataset-Case-1": \
+         "/data/d1/jgwak1/tabby/SILKETW_benign_train_test_data_case1/offline_test/Processed_Benign_ONLY_TaskName_edgeattr",
+      # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      # Dataset-2 (B#167, M#158)
+      "Dataset-Case-2": \
+         "/data/d1/jgwak1/tabby/SILKETW_benign_train_test_data_case1_case2/offline_test/Processed_Benign_ONLY_TaskName_edgeattr"
+    }
+    projection_datapath_Malware_Test_dict = {
+      # Dataset-1 (B#73, M#62) ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      "Dataset-Case-1": \
+         "/data/d1/jgwak1/tabby//SILKETW_malware_train_test_data_case1/offline_test/Processed_Malware_ONLY_TaskName_edgeattr",
+      # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      # Dataset-2 (B#167, M#158)
+      "Dataset-Case-2": \
+         "/data/d1/jgwak1/tabby/SILKETW_malware_train_test_data_case1_case2/offline_test/Processed_Malware_ONLY_TaskName_edgeattr"
+    }
+
+    _dim_node = 5 #46   # num node features ; the #feats
+    _dim_edge = 62 #72    # (or edge_dim) ; num edge features
+
+    _benign_train_data_path = projection_datapath_Benign_Train_dict["Dataset-Case-2"]
+    _malware_train_data_path = projection_datapath_Malware_Train_dict["Dataset-Case-2"]
+    _benign_final_test_data_path = projection_datapath_Benign_Test_dict["Dataset-Case-2"]
+    _malware_final_test_data_path = projection_datapath_Malware_Test_dict["Dataset-Case-2"]
+
+
+
+    # Load both benign and malware graphs """
+    dataprocessor = LoadGraphs()
+    benign_train_dataset = dataprocessor.parse_all_data(_benign_train_data_path, _dim_node, _dim_edge, drop_dim_edge_timescalar = False)
+    malware_train_dataset = dataprocessor.parse_all_data(_malware_train_data_path, _dim_node, _dim_edge, drop_dim_edge_timescalar = False)
+    #train_dataset = benign_train_dataset + malware_train_dataset
+    #print('+ train data loaded #Malware = {} | #Benign = {}'.format(len(malware_train_dataset), len(benign_train_dataset)), flush=True)
+
+    # Load test benign and malware graphs """
+    benign_test_dataset = dataprocessor.parse_all_data(_benign_final_test_data_path, _dim_node, _dim_edge, drop_dim_edge_timescalar = False)
+    malware_test_dataset = dataprocessor.parse_all_data(_malware_final_test_data_path, _dim_node, _dim_edge, drop_dim_edge_timescalar = False)
+    #final_test_dataset = benign_test_dataset + malware_test_dataset
+   # print('+ final-test data loaded #Malware = {} | #Benign = {}'.format(len(malware_test_dataset), len(benign_test_dataset)), flush=True)
+
+
+
+    benign_train_dataset__no_graph_structure_dict = get_No_Graph_Structure_eventdist_dict( dataset= benign_train_dataset )  
+    malware_train_dataset__no_graph_structure_dict = get_No_Graph_Structure_eventdist_dict( dataset= malware_train_dataset )  
+    more_frequent_in_benign, more_frequent_in_malware,common_events_same_count, common_events = eventdist(benign_train_dataset__no_graph_structure_dict,malware_train_dataset__no_graph_structure_dict)
+    with open('/home/pwakodi1/tabby/Graph_embedding_aka_signal_amplification_files/Analysis_results/benign_vs_malware_train_eventdist_result_case2.json', 'w') as file:
+      json.dump({'more_frequent_in_benign': more_frequent_in_benign, 'more_frequent_in_malware': more_frequent_in_malware,'common_events_same_count':common_events_same_count, 'common_events':common_events}, file)
+    
+    benign_test_dataset__no_graph_structure_dict = get_No_Graph_Structure_eventdist_dict( dataset= benign_test_dataset )  
+    malware_test_dataset__no_graph_structure_dict = get_No_Graph_Structure_eventdist_dict( dataset= malware_test_dataset )  
+    more_frequent_in_benign, more_frequent_in_malware, common_events_same_count, common_events = eventdist(benign_test_dataset__no_graph_structure_dict,malware_test_dataset__no_graph_structure_dict)
+    with open('/home/pwakodi1/tabby/Graph_embedding_aka_signal_amplification_files/Analysis_results/benign_vs_malware_test_eventdist_result_case2.json', 'w') as file:
+      json.dump({'more_frequent_in_benign': more_frequent_in_benign, 'more_frequent_in_malware': more_frequent_in_malware, 'common_events_same_count':common_events_same_count, 'common_events':common_events}, file)
+    
+    #print(difference_dict)
